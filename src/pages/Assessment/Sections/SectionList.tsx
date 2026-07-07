58f4ac7ff8d6d1c20d313b12df61f3bbd629c050
anil6200 <anilkumar074042@gmail.com>
2026-07-07 16:08:43 +0530

    fix sectionList Allignment

--- src/pages/Assessment/Sections/SectionList.tsx
+++ src/pages/Assessment/Sections/SectionList.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Trash2, Plus,Edit3 } from "lucide-react";
+import { Trash2, Plus, Edit3 } from "lucide-react";
 import { useSectionList } from "./useSectionList";
-import LoadingSpinner from "../../../components/UI/loadingSpinner/LoadingSpinner"; 
+import LoadingSpinner from "../../../components/UI/loadingSpinner/LoadingSpinner";
 
 const SectionList = () => {
   const { sections, isLoading, handleDelete, getParentName } = useSectionList();
@@ -18,7 +18,7 @@ const SectionList = () => {
 
         <div className="flex items-center w-full sm:w-auto justify-end">
           <Link
-            to="/assessment-sections/add"  
+            to="/assessment-sections/add"
             className="bg-[#0D80F2] text-white px-5 py-2.5 rounded-xl hover:bg-blue-600 transition-colors flex items-center gap-2 font-medium shadow-sm"
           >
             <Plus className="w-5 h-5" /> Add New Section
@@ -26,15 +26,15 @@ const SectionList = () => {
         </div>
       </div>
 
-      <div className="bg-white rounded-xl shadow-2xl   overflow-hidden mb-10">
-        <div className="flex justify-between px-6 py-4 bg-gray-50 text-[11px] font-bold text-gray-600 uppercase border-b border-gray-200">
-          <div className="flex gap-4 w-full">
-            <span className="w-8">S.N</span>
-            <span className="w-48">Section Name</span>
-            <span className="w-48">Under </span>
-            <span className="flex-1">Short Name</span>
-          </div>    
-          <div className="flex gap-10 min-w-50 justify-end">
+      <div className="bg-white rounded-xl shadow-2xl overflow-hidden mb-10">
+        <div className="flex justify-between items-center px-6 py-4 bg-gray-50 text-[11px] font-bold text-gray-600 uppercase border-b border-gray-200">
+          <div className="flex gap-4 flex-1 items-center">
+            <span className="w-12">S.N</span>
+            <span className="flex-1">Section Name</span>
+            <span className="w-64">Under</span>
+            <span className="w-36">Short Name</span>
+          </div>
+          <div className="flex gap-6 items-center justify-end w-48">
             <span className="w-20 text-center">Max Score</span>
             <span className="w-20 text-right">Actions</span>
           </div>
@@ -62,23 +62,26 @@ const SectionList = () => {
                 key={section._id}
                 className="flex justify-between items-center px-6 py-4 border-b border-gray-100 hover:bg-blue-50/50 transition-colors"
               >
-                <div className="flex gap-4 w-full items-center">
-                  <span className="w-8 text-sm font-bold text-gray-500">
+                {/* Left Side Data */}
+                <div className="flex gap-4 flex-1 items-center">
+                  <span className="w-12 text-sm font-bold text-gray-500">
                     {index + 1}
                   </span>
-                 <span className="flex-1 min-w-0 pr-4 text-[14.5px] font-semibold text-gray-700 break-words whitespace-normal leading-tight">
-  {section.section}
-</span>
-                  <span className="w-48 text-[13px] font-medium text-gray-500">
-                  
-                    {getParentName(section.underSection)}
+                  <span className="flex-1 min-w-0 pr-4 text-[14.5px] font-semibold text-gray-700 wrap-break-word whitespace-normal leading-tight">
+                    {section.section}
                   </span>
-                  <span className="flex-1 text-[13px] font-medium text-blue-500 bg-blue-50 px-2 py-1 rounded w-fit">
-                    {section.shortName}
+                  <span className="w-64 text-[13px] font-medium text-gray-500 wrap-break-word pr-2">
+                    {getParentName(section.underSection) || "----"}
                   </span>
+                  <div className="w-36">
+                    <span className="text-[13px] font-medium text-blue-500 bg-blue-50 px-2 py-1 rounded inline-block max-w-full truncate">
+                      {section.shortName}
+                    </span>
+                  </div>
                 </div>
 
-                <div className="flex gap-10 min-w-50 justify-end items-center">
+                {/* Right Side Data */}
+                <div className="flex gap-6 items-center justify-end w-48">
                   <div className="w-20 text-center">
                     {section.maxScore ? (
                       <span className="font-bold text-[14px] text-gray-700">
@@ -89,11 +92,17 @@ const SectionList = () => {
                     )}
                   </div>
 
-                  <button onClick={() => navigate(`/assessment-sections/edit/${section._id}`)} className="text-gray-400 hover:text-blue-500 transition-colors p-1" title="Edit Section">
+                  <div className="w-20 flex justify-end items-center gap-2">
+                    <button
+                      onClick={() =>
+                        navigate(`/assessment-sections/edit/${section._id}`)
+                      }
+                      className="text-gray-400 hover:text-blue-500 transition-colors p-1"
+                      title="Edit Section"
+                    >
                       <Edit3 className="w-4.5 h-4.5" />
                     </button>
 
-                  <div className="w-20 flex justify-end gap-2">
                     <button
                       onClick={() => handleDelete(section._id)}
                       className="text-gray-400 hover:text-red-500 transition-colors p-1"
